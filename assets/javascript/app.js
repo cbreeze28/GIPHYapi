0028c968f5cc547038ddb300d1f6d4d9c7f01b23
Chris B <38058127+cbreeze28@users.noreply.github.com>
2018-06-26 17:26:21 -0400

    pulling 10 objects from GIPHY API

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,35 +1,31 @@
-      var topics = ["Steak", "Mashed Potatoes", "Green Beans", "Chicken Fajitas"];
+var topics = ["Steak", "Mashed Potatoes", "Green Beans", "Chicken Fajitas"];
 
-      function displayFoodInfo() {
+      function displayFoodGif() {
 
         var food = $(this).attr("data-name");
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + food + "&api_key=4VWsY12eftOPiPWRl8E3a7ZxUSWF4Y8f";
-            
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + food + "&api_key=4VWsY12eftOPiPWRl8E3a7ZxUSWF4Y8f&limit=10";
+            console.log(queryURL);
         $.ajax({
           url: queryURL,
           method: "GET"
         }).then(function(response) {
-
+          //I think I need to do a JSON parse of some kind
+          //Response.data returns undefined and no gif's, BUT it does return 10, undefined objects so there's communication
           var foodDiv = $("<div class='food'>");
-          var rating = response.Rated;
+          var rating = response.data;
           var facts = $("<p>").text("Rating: " + rating);
-            foodDiv.prepend(facts);
-
-          var gifURL = response.data.images;
-
-          var gif = $("<gif>").attr("src", gifURL);
-
+            foodDiv.append(facts);
+          var gifURL = response.Poster;
+                console.log(gifURL);
+          var gif = $("<img>").attr("src", gifURL);
           foodDiv.append(gif);
-
-          $("#giphyList").append(foodDiv);
+          $("#giphyList").prepend(foodDiv);
         });
-
       }
 
+      //Function to add new buttons
       function renderButtons() {
-
         $("#buttonsList").empty();
-
         for (var i = 0; i < topics.length; i++) {
           var x = $("<button>");
           x.addClass("food-btn");
@@ -38,11 +34,13 @@
           $("#buttonsList").append(x);
         }
       }
+
+
       $("#addGiphy").on("click", function(event) {
         event.preventDefault();
         var food = $("#giphyInput").val().trim();
         topics.push(food);
         renderButtons();
       });
-      $(document).on("click", ".food-btn", displayFoodInfo);
+      $(document).on("click", ".food-btn", displayFoodGif);
       renderButtons();
