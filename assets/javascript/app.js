82b23461398c249a65eaba0ea0fe9040043f6307
Chris B <38058127+cbreeze28@users.noreply.github.com>
2018-10-25 12:28:31 -0400

    make it better

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,46 +1,125 @@
-var topics = ["Steak", "Mashed Potatoes", "Green Beans", "Chicken Fajitas"];
+$(document).ready(function(){
 
-      function displayFoodGif() {
+  //initial array of tv shows
+  var character = ["Master Chief", "Cortana", "Mario", "Kratos", "Link", "Bowser", "Solid Snake", "Niko Bellic", "Agent 47"]
+  GIFArea = " "
+  
+  //==================RENDER BUTTON=============================
+  
+  //function for displaying tv show data
+  function renderButtons() {
+  
+  //deleting the movie buttons prior to adding new movie buttons
+  $("#character-view").empty();
+  
+  //looping through the array of characters
+  for (var i=0; i < character.length; i++) {
+  
+  //dynamically generate buttons for each movie in the array.
+  //this code $("<button>") is all jquery needs to create the start and end tag. (<button></button>)
+  var a = $('<button>');
+  //Adding a class
+  a.addClass('movie');
+  
+  //adding a data-attribute with a value of the television at index i
+  a.attr('data-name', character[i]);
+  //providing the button's text with a value of the movie at index i
+  a.text(character[i]);
+  //adding the button the html
+  $("#character-view").append(a);
+  }
+  s=
+  $("#character-input").focus();
+  
+  }
+  
+  renderButtons();
+  
+  //============CLICK BUTTON/ LISTENERS==================================
+  //this function handles events where one button is clicked
+  $("#add-character").on('click', function() {
+  
+  //event.preventDefault() prevents the form from trying to submit itself
+  //we're using a form so that the user can hit enter instread of clicking the button
+  event.preventDefault();
+  
+  //This line will grab the text from the input box
+  var character1 = $("#character-input").val().trim();
+  
+  //this movie from the textbox is then added to our array
+  character.push(character1);
+  
+  //calling renderButtons which handles the processing of our movie array
+  renderButtons();
+  
+  });
+  
+  //==============DISPLAY INFO==============================
+    $(document).on('click', 'button',  function() {
 
-        var food = $(this).attr("data-name");
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + food + "&api_key=4VWsY12eftOPiPWRl8E3a7ZxUSWF4Y8f&limit=10";
-            console.log(queryURL);
-        $.ajax({
-          url: queryURL,
-          method: "GET"
-        }).then(function(response) {
-          //I think I need to do a JSON parse of some kind--couldn't get stringify to work
-          //Response.data returns undefined and no gif's, BUT it does return 10, undefined objects so there's communication.
-          var foodDiv = $("<div class='food'>");
-          var rating = response.data;
-          var facts = $("<p>").text("Rating: " + rating);
-            foodDiv.append(facts);
-          var gifURL = response.Poster;
-                console.log(gifURL);
-          var gif = $("<img>").attr("src", gifURL);
-          foodDiv.append(gif);
-          $("#giphyList").prepend(foodDiv);
+          // (this is necessary otherwise we will have repeat buttons)
+        $('#GIFArea').empty(); 
+            var b = $(this).attr('data-name');		// 'this' refers to the button that was clicked
+            var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + b + "&api_key=dc6zaTOxFJmzC&limit=12";  //query api url and public key
+            console.log(queryURL); 
+  
+            //standard ajax call to get request
+            $.ajax({
+                    url: queryURL,
+                    method: 'GET'
+                })
+            //after the data comes back from the API
+                .done(function(response) {
+                    console.log(response);
+          //Storing an array of results in the results variable
+                    var results = response.data;
+            //Looping over every result item
+                    for (var i = 0; i < results.length; i++) {
+            //creating a div with the class item
+                    var gifDiv = $('<div class="item">');
+            //storing the result items rating        
+                    var rating = results[i].rating;
+             //creating an element to have the rating displayed
+              var r = $('<p>').text("Rating: " + rating);
+          //creating a image tag
+              var gifImage = $('<img>');
+          //giving the image tag an src attribute of a property pulled off the result item
+                gifImage.attr('src', results[i].images.fixed_height_still.url)
+                            .attr('data-still', results[i].images.fixed_height_still.url)
+                            .attr('data-animate', results[i].images.fixed_height.url)
+                            .attr('data-state', "still")
+                            .addClass("showImage");
+          //displaying the rating & image
+                        gifDiv.append(r)
+                            .append(gifImage);	                    
+  
+            //prepending data not necessary since cleared             	  
+                        $('#GIFArea').prepend(gifDiv);
+                    }
+  
+                });
         });
-      }
-
-      //Function to add new buttons
-      function renderButtons() {
-        $("#buttonsList").empty();
-        for (var i = 0; i < topics.length; i++) {
-          var x = $("<button>");
-          x.addClass("food-btn");
-          x.attr("data-name", topics[i]);
-          x.text(topics[i]);
-          $("#buttonsList").append(x);
+  
+  
+  //====================Still and Animate Image ==================================
+    // Listens for a click on any image (dynamic)
+    // $('.showImage').on('click', function(){ --> won't work here
+    $(document).on('click', '.showImage',  function() {
+  
+        var state = $(this).data('state');
+      //If the clicked image's state is still, update its src attribute to what its data-animate value is
+        if (state == "still") {
+            console.log("still image works");
+         // Then, set the image's data-state to animate
+            $(this).attr('src', $(this).data('animate'))
+                   .data('state', 'animate');
+        } else {
+        //  else set src to the data-still value
+            console.log("animated image works");
+            $(this).attr('src', $(this).data('still'))
+                   .data('state', 'still');               
         }
-      }
-
-
-      $("#addGiphy").on("click", function(event) {
-        event.preventDefault();
-        var food = $("#giphyInput").val().trim();
-        topics.push(food);
-        renderButtons();
-      });
-      $(document).on("click", ".food-btn", displayFoodGif);
-      renderButtons();
+  
+    });
+  
+  });
